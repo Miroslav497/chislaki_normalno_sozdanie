a77f286c285f4eb2b134a12584ab6da0a5a6ad9e
Miroslav <bitgithar@mail.ru>
2023-11-16 23:17:47 +0300

    laba2

---
+++ laba2/Niutonmethod.h
@@ -0,0 +1,65 @@
+#pragma once 
+
+#include <vector>
+#include "gauss.h"
+#include <cmath>
+
+double function1(const double& x1, const double& x2) {
+    return x1 - x2 - 6*log(x1) - 1;
+}
+
+double function2(const double& x1, const double& x2) {
+    return x1 - 3*x2 - 6*log(x2)-2;
+}
+
+double getD1(const vector<double>& x) {
+    double f1 = function1(x[0], x[1]);
+    double f2 = function2(x[0], x[1]);
+    return (abs(f1)> abs(f2)? abs(f1): abs(f2));//max znach f1 ili f2
+}
+
+double getD2(const vector<double>& x, const vector<double>& xKplus1) {
+    double max = 0;
+    for (int i = 0; i < x.size(); i++) {
+        if (abs(xKplus1[i]) < 1) {
+            if (abs(xKplus1[i] - x[i]) > max)
+                max = abs(xKplus1[i] - x[i]);
+        } else {
+            if (abs((xKplus1[i] - x[i]) / xKplus1[i]) > max)
+                max = abs((xKplus1[i] - x[i]) / xKplus1[i]);
+        }
+    }
+    return max;
+}
+
+
+
+double J1Dx01(const double& x1, const double& x2) {
+    return 1 - 6/x1;
+}
+
+double J1Dx02(const double& x1, const double& x2) {
+    return -1;
+}
+
+double J2Dx01(const double& x1, const double& x2) {
+    return 1;
+}
+
+double J2Dx02(const double& x1, const double& x2) {
+    return -3 - 6/x2;
+}
+
+void jakobianMethod1(vector<vector<double>>& matrix, const vector<double>& x) {
+    matrix[0][0] = J1Dx01(x[0], x[1]);
+    matrix[0][1] = J1Dx02(x[0], x[1]);
+    matrix[1][0] = J2Dx01(x[0], x[1]);
+    matrix[1][1] = J2Dx02(x[0], x[1]);
+}
+
+void jakobianMethod2(vector<vector<double>>& matrix, const vector<double>& x, const double& M) {
+    matrix[0][0] = (function1(x[0] + M * x[0], x[1]) - function1(x[0], x[1])) / M * x[0];
+    matrix[0][1] = (function1(x[0], x[1] + M * x[1]) - function1(x[0], x[1])) / M * x[1];
+    matrix[1][0] = (function2(x[0] + M * x[0], x[1]) - function2(x[0], x[1])) / M * x[0];
+    matrix[1][1] = (function2(x[0], x[1] + M * x[1]) - function2(x[0], x[1])) / M * x[1];
+}
---
+++ laba2/mainfun.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include "Niuton.h"
+using namespace std;
+
+
+
+int main() {
+    vector<double> MParametr = {0.0,0.01, 0.05, 0.1};
+    for (const double& M : MParametr) {
+        cout << "MParametr = " << M << endl;
+        NiutonMethod(M);
+        cout << "--------------------------" << endl;
+    }
+
+    return 0;
+}
